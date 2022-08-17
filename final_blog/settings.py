6e79bc974f6698355bf4991bbf3987e77c0236aa
galikhan <galihan.abdikarim@gmail.com>
2022-08-17 12:38:04 +0600

    fix secret_code

--- final_blog/settings.py
+++ final_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*biva8w%y%#v%2)+n#_99$g48m1a-llx+*izf6sxs)#058*-kf'
+SECRET_KEY = os.getenv('SECRET_CODE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
